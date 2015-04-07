702d41c2f07a66a29871d9ff50abf7f7179ffa6a
Joe Hegarty <joe@bioware.com>
2015-04-07 00:49:37 -0600

    Remove Bridge for now

--- bridge/src/main/java/com/ea/orbit/bridge/OrbitBridgeModule.java
+++
@@ -1,7 +0,0 @@
-package com.ea.orbit.bridge;
-
-import com.ea.orbit.container.Module;
-
-public class OrbitBridgeModule extends Module {
-
-}
--- samples/bridge/src/main/java/com/ea/orbit/samples/bridge/SampleData.java
+++
@@ -1,9 +0,0 @@
-package com.ea.orbit.samples.bridge;
-
-import java.io.Serializable;
-
-public class SampleData implements Serializable {
-    public String item;
-    public int someValue;
-    public String anotherValue;
-}
