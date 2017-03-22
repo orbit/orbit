31ae613eb87a9c89d1403e962044976d04719a67
Chris Gander <cgander@ea.com>
2017-03-22 08:35:00 -0400

    refactored JavaMessageSerializer to make subclassing possible

--- actors/runtime/src/main/java/cloud/orbit/actors/runtime/JavaMessageSerializer.java
+++ actors/runtime/src/main/java/cloud/orbit/actors/runtime/JavaMessageSerializer.java
@@ -28,7 +28,6 @@
 
 package cloud.orbit.actors.runtime;
 
-import cloud.orbit.actors.ActorObserver;
 import cloud.orbit.actors.cluster.NodeAddress;
 import cloud.orbit.actors.cluster.NodeAddressImpl;
 import cloud.orbit.actors.extensions.MessageSerializer;
@@ -36,11 +35,8 @@ import cloud.orbit.actors.extensions.MessageSerializer;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInput;
-import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.io.Serializable;
 import java.util.Map;
 import java.util.UUID;
 
@@ -91,84 +87,15 @@ public class JavaMessageSerializer implements MessageSerializer
         out.writeObject(message.getPayload());
     }
 
-    private static class ReferenceReplacement implements Serializable
-    {
-        private static final long serialVersionUID = 1L;
-
-        Class<?> interfaceClass;
-        Object id;
-        NodeAddress address;
-    }
-
     protected ObjectOutput createObjectOutput(final BasicRuntime runtime, final OutputStream outputStream) throws IOException
     {
         // Message(messageId, type, reference, params) and Message(messageId, type, object)
-        return new ObjectOutputStream(outputStream)
-        {
-            {
-                enableReplaceObject(true);
-            }
-
-            @SuppressWarnings("rawtypes")
-            @Override
-            protected Object replaceObject(final Object obj) throws IOException
-            {
-                final RemoteReference reference;
-                if (!(obj instanceof RemoteReference))
-                {
-                    if (obj instanceof AbstractActor)
-                    {
-                        reference = ((AbstractActor) obj).reference;
-                    }
-                    else if (obj instanceof ActorObserver)
-                    {
-                        ActorObserver objectReference = runtime.registerObserver(null, (ActorObserver) obj);
-                        reference = (RemoteReference) objectReference;
-                    }
-                    else
-                    {
-                        return super.replaceObject(obj);
-                    }
-                }
-                else
-                {
-                    reference = (RemoteReference) obj;
-                }
-                ReferenceReplacement replacement = new ReferenceReplacement();
-                replacement.address = reference.address;
-                replacement.interfaceClass = reference._interfaceClass();
-                replacement.id = reference.id;
-                return replacement;
-            }
-        };
+        return new OrbitObjectOutputStream(outputStream, runtime);
     }
 
     protected ObjectInput createObjectInput(final BasicRuntime runtime, InputStream in) throws IOException
     {
-        return new ObjectInputStream(in)
-        {
-            {
-                enableResolveObject(true);
-            }
-
-            @SuppressWarnings({ "unchecked", "rawtypes" })
-            @Override
-            protected Object resolveObject(Object obj) throws IOException
-            {
-                if (obj instanceof ReferenceReplacement)
-                {
-                    ReferenceReplacement replacement = (ReferenceReplacement) obj;
-                    if (replacement.address != null)
-                    {
-                        return runtime.getRemoteObserverReference(replacement.address, (Class) replacement.interfaceClass, replacement.id);
-                    }
-                    return runtime.getReference((Class) replacement.interfaceClass, replacement.id);
-
-                }
-                return super.resolveObject(obj);
-            }
-        };
+        return new OrbitObjectInputStream(in, runtime);
     }
-
 
 }
