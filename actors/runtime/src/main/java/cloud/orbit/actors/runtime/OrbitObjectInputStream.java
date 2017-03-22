228f66d9ced13130c0cdc9a4cf42248e9d19f83e
Chris Gander <cgander@ea.com>
2017-03-22 08:35:00 -0400

    refactored JavaMessageSerializer to make it more extensible

---
+++ actors/runtime/src/main/java/cloud/orbit/actors/runtime/OrbitObjectInputStream.java
@@ -0,0 +1,63 @@
+/*
+ Copyright (C) 2017 Electronic Arts Inc.  All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1.  Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2.  Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3.  Neither the name of Electronic Arts, Inc. ("EA") nor the names of
+     its contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY ELECTRONIC ARTS AND ITS CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ELECTRONIC ARTS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cloud.orbit.actors.runtime;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+
+public class OrbitObjectInputStream extends ObjectInputStream
+{
+
+    private final BasicRuntime runtime;
+
+    public OrbitObjectInputStream(final InputStream in, final BasicRuntime runtime) throws IOException
+    {
+        super(in);
+        this.runtime = runtime;
+        enableResolveObject(true);
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    @Override
+    protected Object resolveObject(Object obj) throws IOException
+    {
+        if (obj instanceof ReferenceReplacement)
+        {
+            final ReferenceReplacement replacement = (ReferenceReplacement) obj;
+            if (replacement.address != null)
+            {
+                return runtime.getRemoteObserverReference(replacement.address, (Class) replacement.interfaceClass, replacement.id);
+            }
+            return runtime.getReference((Class) replacement.interfaceClass, replacement.id);
+
+        }
+        return super.resolveObject(obj);
+    }
+}
