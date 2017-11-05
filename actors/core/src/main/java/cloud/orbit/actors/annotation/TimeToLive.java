f3cedcea955c33260b79aec6b119656ee5abed48
Joe Hegarty <joeh@ea.com>
2017-11-05 15:32:10 -0800

    Javadoc comments

--- actors/core/src/main/java/cloud/orbit/actors/annotation/TimeToLive.java
+++ actors/core/src/main/java/cloud/orbit/actors/annotation/TimeToLive.java
@@ -34,6 +34,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Defines a custom time to live for the given actor that will be used instead of the
+ * configured <code>defaultActorTTL</code>.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 public @interface TimeToLive
