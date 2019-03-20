c1edd9eb582be1cc3607ace578d48d3ca07fe512
Joe Hegarty <joeh@ea.com>
2019-03-19 22:40:41 -0700

    Update Copyright

--- src/dsl/orbit-codegen/src/main/kotlin/cloud/orbit/dsl/java/JavaCodeGenerator.kt
+++ src/dsl/orbit-codegen/src/main/kotlin/cloud/orbit/dsl/java/JavaCodeGenerator.kt
@@ -1,6 +1,6 @@
 /*
- Copyright (C) 2015 - 2018 Electronic Arts Inc.  All rights reserved.
- This file is part of the Orbit Project <http://www.orbit.cloud>.
+ Copyright (C) 2015 - 2019 Electronic Arts Inc.  All rights reserved.
+ This file is part of the Orbit Project <https://www.orbit.cloud>.
  See license in LICENSE.
  */
 
