e3058a40de48070187194f85d2c7ba5f7c88a50f
Joe Hegarty <joeh@ea.com>
2019-04-04 19:34:06 -0700

    Optimization

--- src/framework/orbit-runtime/src/main/kotlin/cloud/orbit/runtime/serialization/kryo/JDKKryoSerializers.kt
+++ src/framework/orbit-runtime/src/main/kotlin/cloud/orbit/runtime/serialization/kryo/JDKKryoSerializers.kt
@@ -16,12 +16,15 @@ internal class MethodSerializer: Serializer<Method>(false, true) {
     override fun write(kryo: Kryo, output: Output, method: Method) {
         kryo.writeClass(output, method.declaringClass)
         output.writeString(method.name)
+        kryo.writeClassAndObject(output, method.parameterTypes)
     }
 
     override fun read(kryo: Kryo, input: Input, type: Class<out Method>): Method {
         val clazz = kryo.readClass(input).type
         val methodName = input.readString()
-        return clazz.methods.single { it.name == methodName }
+        @Suppress("UNCHECKED_CAST")
+        val params = kryo.readClassAndObject(input) as Array<Class<*>>
+        return clazz.getDeclaredMethod(methodName, *params)
     }
 
     override fun copy(kryo: Kryo, original: Method): Method = original
