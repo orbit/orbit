edf1739fbfe0ab13266bb606594aa69d5d07ea5c
Cesar Blum Silveira <cesarbs@gmail.com>
2019-05-15 10:30:00 -0700

    DSL: support void as a primitive type (#387)

--- src/dsl/orbit-dsl-java/src/test/kotlin/cloud/orbit/dsl/java/TypeIndexerTest.kt
+++ src/dsl/orbit-dsl-java/src/test/kotlin/cloud/orbit/dsl/java/TypeIndexerTest.kt
@@ -17,62 +17,63 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 
 class TypeIndexerTest {
-    private val expectedPredefinedTypes = mapOf(
-        TypeReference("boolean") to TypeName.BOOLEAN,
-        TypeReference("double") to TypeName.DOUBLE,
-        TypeReference("float") to TypeName.FLOAT,
-        TypeReference("int32") to TypeName.INT,
-        TypeReference("int64") to TypeName.LONG,
-        TypeReference("string") to ClassName.get(String::class.java),
-        TypeReference("list") to ClassName.get(java.util.List::class.java),
-        TypeReference("map") to ClassName.get(java.util.Map::class.java)
-    )
-
     @Test
     fun indexDefaultTypes() {
-        val actual = TypeIndexer().visitCompilationUnits(emptyList())
-        Assertions.assertEquals(expectedPredefinedTypes, actual)
+        val expectedPredefinedTypes = mapOf(
+            TypeReference("boolean") to TypeName.BOOLEAN,
+            TypeReference("double") to TypeName.DOUBLE,
+            TypeReference("float") to TypeName.FLOAT,
+            TypeReference("int32") to TypeName.INT,
+            TypeReference("int64") to TypeName.LONG,
+            TypeReference("string") to ClassName.get(String::class.java),
+            TypeReference("void") to ClassName.get(Void::class.java),
+            TypeReference("list") to ClassName.get(java.util.List::class.java),
+            TypeReference("map") to ClassName.get(java.util.Map::class.java)
+        )
+
+        val types = TypeIndexer().visitCompilationUnits(emptyList())
+        Assertions.assertEquals(expectedPredefinedTypes, types)
     }
 
     @Test
     fun indexEnum() {
-        val actual = TypeIndexer().visitCompilationUnits(
+        val types = TypeIndexer().visitCompilationUnits(
             listOf(
                 CompilationUnit("foo", enums = listOf(EnumDeclaration("bar")))
             )
         )
 
-        val expectedTypes = mutableMapOf<TypeReference, TypeName>(TypeReference("bar") to ClassName.get("foo", "bar"))
-        expectedTypes.putAll(expectedPredefinedTypes)
-
-        Assertions.assertEquals(expectedTypes, actual)
+        Assertions.assertEquals(
+            ClassName.get("foo", "bar"),
+            types[TypeReference("bar")]
+        )
     }
 
     @Test
     fun indexData() {
-        val actual = TypeIndexer().visitCompilationUnits(
+        val types = TypeIndexer().visitCompilationUnits(
             listOf(
                 CompilationUnit("foo", data = listOf(DataDeclaration("bar")))
             )
         )
 
-        val expectedTypes = mutableMapOf<TypeReference, TypeName>(TypeReference("bar") to ClassName.get("foo", "bar"))
-        expectedTypes.putAll(expectedPredefinedTypes)
-
-        Assertions.assertEquals(expectedTypes, actual)
+        Assertions.assertEquals(
+            ClassName.get("foo", "bar"),
+            types[TypeReference("bar")]
+        )
     }
 
     @Test
     fun indexActor() {
-        val actual = TypeIndexer().visitCompilationUnits(
+        val types = TypeIndexer().visitCompilationUnits(
             listOf(
                 CompilationUnit("foo", actors = listOf(ActorDeclaration("bar")))
             )
         )
 
-        val expectedTypes = mutableMapOf<TypeReference, TypeName>(TypeReference("bar") to ClassName.get("foo", "bar"))
-        expectedTypes.putAll(expectedPredefinedTypes)
-
-        Assertions.assertEquals(expectedTypes, actual)
+        Assertions.assertEquals(
+            ClassName.get("foo", "bar"),
+            types[TypeReference("bar")]
+        )
     }
 }
