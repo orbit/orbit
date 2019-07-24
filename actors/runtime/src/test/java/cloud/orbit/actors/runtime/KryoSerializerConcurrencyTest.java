orbit/orbit

commit 5735ac14981fc7f61144c6364c320ad5d697067c

Add ability to pass a custom ClassResolver to the KryoSerializer * Kept existing constructor for KryoSerializer * Added an additional constructor to KryoSerializer which allows passing a ClassResolver * Using the delegate pattern in KryoSerializer to inject the same custom "writeClass" method logic as before * Added tests for KryoSerializer to check everything still works

actors/runtime/src/main/java/cloud/orbit/actors/runtime/KryoSerializer.java
@@ -30,6 +30,7 @@ package cloud.orbit.actors.runtime;
 
 import org.objenesis.strategy.StdInstantiatorStrategy;
 
+import com.esotericsoftware.kryo.ClassResolver;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.Registration;
 import com.esotericsoftware.kryo.Serializer;
@@ -91,35 +92,85 @@ public class KryoSerializer implements ExecutionObjectCloner, MessageSerializer
 
     public KryoSerializer(Consumer<Kryo> kryoConsumer)
     {
+        this(kryoConsumer, new DefaultClassResolver());
+    }
+
+    public KryoSerializer(Consumer<Kryo> kryoConsumer, ClassResolver classResolver)
+    {
         KryoFactory factory = new KryoFactory()
         {
             @Override
             public Kryo create()
             {
-                Kryo kryo = new Kryo(new DefaultClassResolver()
+                Kryo kryo = new Kryo(new ClassResolver()
                 {
+                    Kryo kryo;
+
                     @Override
-                    public Registration writeClass(Output output, Class type)
+                    public void setKryo(final Kryo kryo)
+                    {
+                        this.kryo = kryo;
+                        classResolver.setKryo(kryo);
+                    }
+
+                    @Override
+                    public Registration register(final Registration registration)
+                    {
+                        return classResolver.register(registration);
+                    }
+
+                    @Override
+                    public Registration registerImplicit(final Class type)
+                    {
+                        return classResolver.registerImplicit(type);
+                    }
+
+                    @Override
+                    public Registration getRegistration(final Class type)
+                    {
+                        return classResolver.getRegistration(type);
+                    }
+
+                    @Override
+                    public Registration getRegistration(final int classID)
+                    {
+                        return classResolver.getRegistration(classID);
+                    }
+
+                    @Override
+                    public Registration writeClass(final Output output, final Class type)
                     {
                         if (type != null && !type.isInterface())
                         {
                             if (ActorObserver.class.isAssignableFrom(type))
                             {
-                                super.writeClass(output, ActorObserver.class);
+                                classResolver.writeClass(output, ActorObserver.class);
                                 return kryo.getRegistration(type);
                             }
                             else if (AbstractActor.class.isAssignableFrom(type))
                             {
-                                super.writeClass(output, AbstractActor.class);
+                                classResolver.writeClass(output, AbstractActor.class);
                                 return kryo.getRegistration(type);
                             }
                             else if (RemoteReference.class.isAssignableFrom(type))
                             {
-                                super.writeClass(output, RemoteReference.class);
+                                classResolver.writeClass(output, RemoteReference.class);
                                 return kryo.getRegistration(type);
                             }
                         }
-                        return super.writeClass(output, type);
+                        return classResolver.writeClass(output, type);
+                    }
+
+                    @Override
+                    public Registration readClass(final Input input)
+                    {
+                        return classResolver.readClass(input);
+                    }
+
+                    @Override
+                    public void reset()
+                    {
+                        classResolver.reset();
                     }
                 }, new MapReferenceResolver(), new DefaultStreamFactory());
 

actors/runtime/pom.xml
@@ -134,5 +134,11 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
             <artifactId>javax.inject</artifactId>
             <version>1</version>
         </dependency>
+        <dependency>
+            <groupId>org.mockito</groupId>
+            <artifactId>mockito-all</artifactId>
+            <version>1.9.5</version>
+            <scope>test</scope>
+        </dependency>
     </dependencies>
 </project>

actors/runtime/src/test/java/cloud/orbit/actors/runtime/KryoSerializerTest.java
@@ -1,0 +1,119 @@
+/*
+ Copyright (C) 2019 Electronic Arts Inc.  All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1.  Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2.  Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3.  Neither the name of Electronic Arts, Inc. ("EA") nor the names of
+     its contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY ELECTRONIC ARTS AND ITS CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ELECTRONIC ARTS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cloud.orbit.actors.runtime;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.esotericsoftware.kryo.Registration;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.util.DefaultClassResolver;
+
+import java.util.Objects;
+import java.util.function.Supplier;
+
+public class KryoSerializerTest
+{
+
+    private void runSerializeDeserializeTest(Supplier<KryoSerializer> kryoSerializerSupplier,
+                                             Object in,
+                                             Object expectedOut) throws Exception
+    {
+        BasicRuntime basicRuntime = Mockito.mock(BasicRuntime.class);
+        KryoSerializer kryoSerializer = kryoSerializerSupplier.get();
+
+        byte[] bytes = kryoSerializer.serializeMessage(basicRuntime, new Message().withPayload(in));
+        Message message = kryoSerializer.deserializeMessage(basicRuntime, bytes);
+
+        Assert.assertEquals(expectedOut, message.getPayload());
+    }
+
+    @Test
+    public void serializeAndDeserialize_withDefaultClassResolver() throws Exception
+    {
+        runSerializeDeserializeTest(KryoSerializer::new, new ExampleDTO("a"), new ExampleDTO("a"));
+    }
+
+    @Test
+    public void serializeAndDeserialize_withCustomClassResolver() throws Exception
+    {
+        runSerializeDeserializeTest(
+                () -> new KryoSerializer(
+                        kryo -> {},
+                        new DefaultClassResolver()
+                        {
+                            @Override
+                            public Registration writeClass(final Output output, final Class type)
+                            {
+                                if (type == ExampleDTO.class)
+                                {
+                                    return super.writeClass(output, ExampleDTOCopy.class);
+                                }
+                                return super.writeClass(output, type);
+                            }
+                        }),
+                new ExampleDTO("a"),
+                new ExampleDTOCopy("a"));
+    }
+
+    private static class ExampleDTO
+    {
+        private final String a;
+
+        public ExampleDTO(final String a)
+        {
+            this.a = a;
+        }
+
+        @Override
+        public boolean equals(final Object o)
+        {
+            if (o == null || getClass() != o.getClass()) return false;
+            return Objects.equals(a, ((ExampleDTO) o).a);
+        }
+    }
+
+    private static class ExampleDTOCopy
+    {
+        private final String a;
+
+        public ExampleDTOCopy(final String a)
+        {
+            this.a = a;
+        }
+
+        @Override
+        public boolean equals(final Object o)
+        {
+            if (o == null || getClass() != o.getClass()) return false;
+            return Objects.equals(a, ((ExampleDTOCopy) o).a);
+        }
+    }
+}

commit 18137c4c4397aa2cbf99b5d0ac2ffac599a20dff

Fix KryoSerializer concurrency bug (introduced in 5735ac14)

actors/runtime/src/main/java/cloud/orbit/actors/runtime/KryoSerializer.java
@@ -71,6 +71,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 /**
  * Kryo based message serializer and object cloning implementation.
@@ -92,16 +93,27 @@ public class KryoSerializer implements ExecutionObjectCloner, MessageSerializer
 
     public KryoSerializer(Consumer<Kryo> kryoConsumer)
     {
-        this(kryoConsumer, new DefaultClassResolver());
+        this(kryoConsumer, DefaultClassResolver::new);
     }
 
+    /**
+     * @deprecated Do not use this constructor, since it will share a single ClassResolver between Kryo instances in the
+     * Kryo pool which can cause concurrency issues.
+     */
+    @Deprecated
     public KryoSerializer(Consumer<Kryo> kryoConsumer, ClassResolver classResolver)
+    {
+        this(kryoConsumer, () -> classResolver);
+    }
+
+    public KryoSerializer(Consumer<Kryo> kryoConsumer, Supplier<ClassResolver> classResolverSupplier)
     {
         KryoFactory factory = new KryoFactory()
         {
             @Override
             public Kryo create()
             {
+                ClassResolver classResolver = classResolverSupplier.get();
                 Kryo kryo = new Kryo(new ClassResolver()
                 {
                     Kryo kryo;

actors/runtime/src/test/java/cloud/orbit/actors/runtime/KryoSerializerConcurrencyTest.java
@@ -1,0 +1,73 @@
+/*
+ Copyright (C) 2019 Electronic Arts Inc.  All rights reserved.
+
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions
+ are met:
+
+ 1.  Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+ 2.  Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in the
+     documentation and/or other materials provided with the distribution.
+ 3.  Neither the name of Electronic Arts, Inc. ("EA") nor the names of
+     its contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+ THIS SOFTWARE IS PROVIDED BY ELECTRONIC ARTS AND ITS CONTRIBUTORS "AS IS" AND ANY
+ EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL ELECTRONIC ARTS OR ITS CONTRIBUTORS BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package cloud.orbit.actors.runtime;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class KryoSerializerConcurrencyTest
+{
+    @Test
+    public void testConcurrentDeserializations() throws Exception
+    {
+        KryoSerializer kryoSerializer = new KryoSerializer();
+        BasicRuntime basicRuntime = Mockito.mock(BasicRuntime.class);
+        ExecutorService executorService = Executors.newFixedThreadPool(16);
+        byte[] bytes = kryoSerializer.serializeMessage(basicRuntime, new Message().withPayload(new MyDTO()));
+        Throwable[] t = new Throwable[]{null};
+        CountDownLatch latch = new CountDownLatch(160);
+        for (int i = 0; i < 160; i++)
+        {
+            executorService.execute(() -> {
+                try
+                {
+                    kryoSerializer.deserializeMessage(basicRuntime, bytes);
+                }
+                catch (Exception e)
+                {
+                    t[0] = e;
+                    e.printStackTrace();
+                }
+                latch.countDown();
+            });
+        }
+        latch.await();
+        Assert.assertNull(t[0]);
+    }
+
+    public class MyDTO
+    {
+        int a;
+    }
+}