5f2e84a7d00f9bfb8364ee0913aecd23b6dca647
Brett Morien <bmorien@ea.com>
2019-08-21 11:40:47 -0700

    Change "Pair" class in Graph Traverser to ParentChild to avoid conflict with built-in type.

--- src/orbit-commons/src/main/kotlin/orbit/common/collections/GraphTraverser.kt
+++ src/orbit-commons/src/main/kotlin/orbit/common/collections/GraphTraverser.kt
@@ -7,13 +7,13 @@
 package orbit.common.collections
 
 class GraphTraverser<T>(val getChildren: (T) -> Sequence<T>) {
-    fun traverse(initial: T): Sequence<Pair<T>> {
+    fun traverse(initial: T): Sequence<ParentChild<T>> {
         return sequence() {
-            var row = listOf(Pair(null, initial))
+            var row = listOf(ParentChild(null, initial))
             do {
                 yieldAll(row)
                 row = row.asSequence().flatMap { node -> getChildren(node.child).map { child ->
-                    Pair(
+                    ParentChild(
                         node.child,
                         child
                     )
@@ -22,7 +22,7 @@ class GraphTraverser<T>(val getChildren: (T) -> Sequence<T>) {
         }
     }
 
-    data class Pair<T>(val parent: T?, val child: T) {
+    data class ParentChild<T>(val parent: T?, val child: T) {
 
     }
 }
