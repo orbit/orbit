e8d3451b8f74a7ad8ed124082bbb90ce284d4ffc
Joe Hegarty <joeh@ea.com>
2019-08-26 15:27:54 -0700

    Blank client project

---
+++ src/orbit-client/src/main/java/orbit/client/OrbitClient.java
@@ -0,0 +1,10 @@
+/*
+ Copyright (C) 2015 - 2019 Electronic Arts Inc.  All rights reserved.
+ This file is part of the Orbit Project <https://www.orbit.cloud>.
+ See license in LICENSE.
+ */
+
+package orbit.client;
+
+public final class OrbitClient {
+}
