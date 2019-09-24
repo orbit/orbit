e7222864067a0b09da0730111582c511afd5334c
Joe Hegarty <joeh@ea.com>
2019-09-24 12:38:28 -0700

    Switch to Zulu 11 in docker images

--- docker/server/entrypoint.sh
+++ docker/server/entrypoint.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env sh
 
-JAVA_HOME=/usr/local/jdk-11
-PATH=$JAVA_HOME/bin:"$PATH"
-
-# Start Marketplace
+# Start Orbit
 cd /opt/orbit
 java -jar ./libs/orbit-application-all-2.0.0-SNAPSHOT.jar
