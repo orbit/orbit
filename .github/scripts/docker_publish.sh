911ed0890ef31d6214408197fa9f62b55cfd92aa
Brett Morien <bmorien@ea.com>
2020-01-08 13:59:25 -0800

    Try a release helm chart workflow, update pathing in release workflow.

--- .github/scripts/docker_publish.sh
+++ .github/scripts/docker_publish.sh
@@ -2,5 +2,5 @@
 
 docker login "$DOCKER_BASE_URL" -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 
-docker build -t "${DOCKER_REPO}/orbit:${ORBIT_VERSION}" -f deploy/docker/server/Dockerfile .
+docker build -t "${DOCKER_REPO}/orbit:${ORBIT_VERSION}" -f docker/server/Dockerfile .
 docker push "${DOCKER_REPO}/orbit:${ORBIT_VERSION}"
