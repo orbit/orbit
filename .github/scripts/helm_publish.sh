e58df467029c73f01326d889e43d3003392233f2
Brett Morien <bmorien@ea.com>
2020-01-09 15:21:17 -0800

    Change cr command to run locally instead of from /usr/local/bin

--- .github/scripts/helm_publish.sh
+++ .github/scripts/helm_publish.sh
@@ -10,8 +10,9 @@ platform="linux"
 curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$crVersion/chart-releaser_${crVersion#v}_${platform}_amd64.tar.gz"
 tar -xzf cr.tar.gz
 rm -f cr.tar.gz
-mv -f cr /usr/local/bin/cr
 
 helm package "$chartDir" --destination .cr-release-packages --dependency-update
 
-cr upload -o "$owner" -r "$repo" -t $token
+./cr upload -o "$owner" -r "$repo" -t $token
+
+rm -f cr
