2c87207b9b82441fac54968483d481c6b9d93f59
Brett Morien <bmorien@ea.com>
2020-01-09 14:00:00 -0800

    Use custom script for publishing helm chart in release workflow

---
+++ .github/scripts/helm_publish.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+crVersion="v0.2.3"
+chartDir="./charts/orbit"
+owner="orbit"
+repo="orbit.githib.com/orbit"
+token=$GITHUB_TOKEN
+platform="linux"
+
+curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$crVersion/chart-releaser_${crVersion#v}_${platform}_amd64.tar.gz"
+tar -xzf cr.tar.gz
+rm -f cr.tar.gz
+mv -f cr /usr/local/bin/cr
+
+helm package "$chartDir" --destination .cr-release-packages --dependency-update
+
+cr upload -o "$owner" -r "$repo" -t $token
