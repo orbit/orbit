3546cd1e2d1bb47e6bdad11c2702a6d278346feb
Brett Morien <bmorien@ea.com>
2020-01-10 11:08:08 -0800

    Some adjustments to the publish helm script.

--- .github/scripts/helm_publish.sh
+++ .github/scripts/helm_publish.sh
@@ -8,19 +8,19 @@ repo="orbit.githib.com/orbit"
 token=$GITHUB_TOKEN
 platform="linux"
 
-curl -sSLo helm.tar.gz https://get.helm.sh/helm-v$helmVersion-linux-386.tar.gz
+curl -sSLo helm.tar.gz https://get.helm.sh/helm-v$helmVersion-$platform-amd64.tar.gz
 tar -xzf helm.tar.gz
 rm -f helm.tar.gz
 
-curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/v$crVersion/chart-releaser_$crVersion_${platform}_amd64.tar.gz"
+curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/v${crVersion}/chart-releaser_${crVersion}_${platform}_amd64.tar.gz"
 tar -xzf cr.tar.gz
 rm -f cr.tar.gz
 
 mkdir .cr-release-packages
-./helm version
-./helm package "$chartDir" --destination .cr-release-packages --dependency-update --save=false
+./$platform-amd64/helm version
+./$platform-amd64/helm package "$chartDir" --destination .cr-release-packages --dependency-update
 
 ./cr upload -o "$owner" -r "$repo" -t $token
 
 rm -f cr
-rm -rf helm
+rm -rf ./$platform-amd64
