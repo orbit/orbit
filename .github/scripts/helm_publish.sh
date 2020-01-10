e83765d41fbf8f71e041aa448423ffb458c3c506
Brett Morien <bmorien@ea.com>
2020-01-10 10:10:30 -0800

    Attempting to use helm 3 in helm publishing script.

--- .github/scripts/helm_publish.sh
+++ .github/scripts/helm_publish.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
-crVersion="v0.2.3"
+crVersion="0.2.3"
+helmVersion="3.0.2"
 chartDir="./charts/orbit"
 owner="orbit"
 repo="orbit.githib.com/orbit"
 token=$GITHUB_TOKEN
 platform="linux"
 
-curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/$crVersion/chart-releaser_${crVersion#v}_${platform}_amd64.tar.gz"
+curl -sSLo helm.tar.gz https://get.helm.sh/helm-v$helmVersion-linux-386.tar.gz
+tar -xzf helm.tar.gz
+rm -f helm.tar.gz
+
+curl -sSLo cr.tar.gz "https://github.com/helm/chart-releaser/releases/download/v$crVersion/chart-releaser_$crVersion_${platform}_amd64.tar.gz"
 tar -xzf cr.tar.gz
 rm -f cr.tar.gz
 
 mkdir .cr-release-packages
-helm version
-helm package "$chartDir" --destination .cr-release-packages --dependency-update --save=false
+./helm version
+./helm package "$chartDir" --destination .cr-release-packages --dependency-update --save=false
 
 ./cr upload -o "$owner" -r "$repo" -t $token
 
 rm -f cr
+rm -rf helm
