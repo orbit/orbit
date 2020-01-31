96227890ccb47974cc2eb14b13d32c0117e07fb9
Brett Morien <bmorien@ea.com>
2020-01-30 16:31:34 -0800

    Fix Github update version script after changing yaml values for image tag.

--- .github/scripts/update_version.sh
+++ .github/scripts/update_version.sh
@@ -5,7 +5,7 @@ sed -i.bak "s/version: .*/version: $version/" charts/orbit/Chart.yaml
 rm -f charts/orbit/Chart.yaml.bak
 git add ./charts/orbit/Chart.yaml
 
-sed -i.bak "s/  version: .*/  version: $version/" charts/orbit/values.yaml
+sed -i.bak "s/tag: 2.*/tag: $version/" charts/orbit/values.yaml
 rm -f charts/orbit/values.yaml.bak
 git add ./charts/orbit/values.yaml
 
