4e42606738c307f7f1a62c8e8a988548f6830d86
Brett Morien <bmorien@ea.com>
2020-03-16 16:18:46 -0700

    Some updates to update release script using jq to extract release variables so they don't overwrite the original values.

--- .github/scripts/update_release.sh
+++ .github/scripts/update_release.sh
@@ -5,6 +5,10 @@ tag=v$TAG_VERSION
 GH_REPO="https://api.github.com/repos/$owner/$repo"
 AUTH="Authorization: token $GITHUB_TOKEN"
 
+# Install jq
+apt-get update
+apt-get install jq
+
 # Commit all changed work
 git commit -m "Release version $tag and update docs" --author="orbit-tools <orbit@ea.com>"
 
@@ -13,18 +17,29 @@ git tag $tag
 git push origin master --tags
 
 # Get commit id
-commitId=$(git rev-parse HEAD)
+commitId=$(git rev-parse --short HEAD)
 echo Commit Id: $commitId
 
 # Read asset tags.
-releaseResponse=$(curl -sH "$AUTH" "$GH_REPO/releases/tags/_$tag")
+release=$(curl -sH "$AUTH" "$GH_REPO/releases/tags/_$tag")
 
-# Extract the release id
-eval $(echo "$releaseResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
-[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$releaseResponse" | awk 'length($0)<100' >&2; exit 1; }
+releaseId=jq .id <<EOF 
+    $release 
+EOF
+
+releaseName=
+jq .name <<EOF
+    $release
+EOF
+
+releaseBody=jq .body <<EOF 
+    $release 
+EOF
+
+echo Release: $releaseId - $releaseName - $releaseBody
 
 # Patch release with new commit Id and tag
-curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_REPO/releases/$id -d '{"tag_name": "$tag", "target_commitish": "$commitId"}'
+curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_REPO/releases/$releaseId -d '{"tag_name": "$tag", "target_commitish": "$commitId", "name":"$releaseName", "body": "$releaseBody", "draft": "false", "prerelease": "false"}'
 
 git tag -d _$tag
 git push origin :refs/tags/_$tag
