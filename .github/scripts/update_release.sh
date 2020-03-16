bc026f572e32aafaa0a6a9621e60cad1ba5657f2
Brett Morien <bmorien@ea.com>
2020-03-16 14:42:27 -0700

    Fix issue with incorrectly constructed release url in build scripts.

--- .github/scripts/update_release.sh
+++ .github/scripts/update_release.sh
@@ -24,8 +24,5 @@ releaseResponse=$(curl -sH "$AUTH" "$GH_REPO/releases/tags/_$tag")
 eval $(echo "$releaseResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
 [ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$releaseResponse" | awk 'length($0)<100' >&2; exit 1; }
 
-# Construct edit release url
-GH_EDIT_RELEASE_URL"$GH_REPO/releases/$id"
-
 # Patch release with new commit Id and tag
-curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_EDIT_RELEASE_URL -d '{"tag_name": "$tag", "target_commitish": "$commitId"}'
+curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_REPO/releases/$id -d '{"tag_name": "$tag", "target_commitish": "$commitId"}'
