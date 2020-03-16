c5d4f3490b91b816424e84cb885615bc32a267b4
Brett Morien <bmorien@ea.com>
2020-03-16 15:25:37 -0700

    Change ordering on tagging commands in update release build script.

--- .github/scripts/update_release.sh
+++ .github/scripts/update_release.sh
@@ -8,8 +8,13 @@ AUTH="Authorization: token $GITHUB_TOKEN"
 # Commit all changed work
 git commit -m "Release version $tag and update docs" --author="orbit-tools <orbit@ea.com>"
 
+# Tag commit with the intended release tag (without the underscore)
+git tag $tag
+git push origin master --tags
+
 # Get commit id
 commitId=$(git rev-parse HEAD)
+echo Commit Id: $commitId
 
 # Read asset tags.
 releaseResponse=$(curl -sH "$AUTH" "$GH_REPO/releases/tags/_$tag")
@@ -21,9 +26,6 @@ eval $(echo "$releaseResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd
 # Patch release with new commit Id and tag
 curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_REPO/releases/$id -d '{"tag_name": "$tag", "target_commitish": "$commitId"}'
 
-# Tag commit with the intended release tag (without the underscore)
-git tag $tag
-git push origin master --tags
 git tag -d _$tag
 git push origin :refs/tags/_$tag
 git reset --hard
