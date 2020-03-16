0b9e073966250c1b6788f61003d9ed091f2ecd29
Brett Morien <bmorien@ea.com>
2020-03-16 11:45:54 -0700

    Update release process to re-tag the release to the commit with automated version updates in files.

---
+++ .github/scripts/update_release.sh
@@ -0,0 +1,31 @@
+owner="orbit"
+repo="orbit"
+
+tag=$TAG_VERSION
+GH_REPO="https://api.github.com/repos/$owner/$repo"
+AUTH="Authorization: token $GITHUB_TOKEN"
+
+# Commit all changed work
+git commit -m "Release version $tag and update docs" --author="orbit-tools <orbit@ea.com>"
+
+# Get commit id
+commitId = $(git rev-parse HEAD)
+
+# Tag commit with the intended release tag (without the underscore)
+git tag "$tag"
+git tag -d "_$tag"
+git push origin master
+git reset --hard
+
+# Read asset tags.
+releaseResponse=$(curl -sH "$AUTH" "$GH_REPO/releases/tags/_$tag")
+
+# Extract the release id
+eval $(echo "$releaseResponse" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
+[ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$releaseResponse" | awk 'length($0)<100' >&2; exit 1; }
+
+# Construct edit release url
+GH_EDIT_RELEASE_URL"$GH_REPO/releases/$id"
+
+# Patch release with new commit Id and tag
+curl -X PATCH -H "$AUTH" -H "Content-Type: application/json" $GH_EDIT_RELEASE_URL -d '{"tag_name": "$tag", "target_commitish": "$commitId"}'
--- .github/scripts/upload_chart.sh
+++ .github/scripts/upload_chart.sh
@@ -4,22 +4,23 @@ done
 
 GH_API="https://api.github.com"
 GH_REPO="$GH_API/repos/$owner/$repo"
-GH_TAGS="$GH_REPO/releases/tags/$tag"
+GH_TAG_URL="$GH_REPO/releases/tags/_$tag"
 AUTH="Authorization: token $github_api_token"
 
 # Validate token.
 curl -o /dev/null -sH "$AUTH" $GH_REPO || { echo "Error: Invalid repo, token or network issue!";  exit 1; }
 
 # Read asset tags.
-response=$(curl -sH "$AUTH" $GH_TAGS)
+response=$(curl -sH "$AUTH" $GH_TAG_URL)
 
+# Extract the release id
 eval $(echo "$response" | grep -m 1 "id.:" | grep -w id | tr : = | tr -cd '[[:alnum:]]=')
 [ "$id" ] || { echo "Error: Failed to get release id for tag: $tag"; echo "$response" | awk 'length($0)<100' >&2; exit 1; }
 
 # Upload asset
 echo "Uploading asset... "
 
-# Construct url
-GH_ASSET="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
+# Construct upload url
+GH_UPLOAD_URL="https://uploads.github.com/repos/$owner/$repo/releases/$id/assets?name=$(basename $filename)"
 
-curl --data-binary @"$filename" -H "Authorization: token $github_api_token" -H "Content-Type: application/octet-stream" $GH_ASSET
+curl --data-binary @"$filename" -H "$AUTH" -H "Content-Type: application/octet-stream" $GH_UPLOAD_URL
