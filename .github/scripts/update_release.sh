216374dc795ad607e8ff7ec38c0a08792af4c4c0
Brett Morien <bmorien@ea.com>
2020-07-01 12:11:21 -0700

    Setup git config info for committing release information.

--- .github/scripts/update_release.sh
+++ .github/scripts/update_release.sh
@@ -7,6 +7,9 @@ tag=v$TAG_VERSION
 GH_REPO="https://api.github.com/repos/$owner/$repo"
 AUTH="Authorization: token $GITHUB_TOKEN"
 
+git config user.email "orbit@ea.com"
+git config user.name "orbit-tools"
+
 # Commit all changed work
 git commit -m "Release version $tag and update docs" --author="orbit-tools <orbit@ea.com>"
 
