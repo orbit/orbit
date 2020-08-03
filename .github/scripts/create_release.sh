1456fe021186f39d83b243c10a49ede5b5a2a5de
Brett Morien <bmorien@ea.com>
2020-08-03 14:02:59 -0700

    Fix incorrect urls in create release script.

--- .github/scripts/create_release.sh
+++ .github/scripts/create_release.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-owner="orbit"
-repo="orbit"
-
 version=$TAG_VERSION
-GH_REPO="https://api.github.com/repos/$owner/$repo"
+GH_REPO="https://api.github.com/repos/orbit/orbit"
 AUTH="Authorization: token $GITHUB_TOKEN"
 
 echo Commit all changed work
 git commit -am "Release version $version and update docs" --author="orbit-tools <orbit@ea.com>"
 
 echo Create release notes from previous commit messages
-echo curl -sH "$AUTH" "$GH_REPO/orbit/orbit/releases" | jq ".[0].created_at"
-curl -sH "$AUTH" "$GH_REPO/orbit/orbit/releases" | jq ".[0].created_at"
-echo curl -sH "$AUTH" "$GH_REPO/orbit/orbit/commits?since=$(curl -sH "$AUTH" "$GH_REPO/orbit/orbit/releases" | jq ".[0].created_at")"
-curl -sH "$AUTH" "$GH_REPO/orbit/orbit/commits?since=$(curl -sH "$AUTH" "$GH_REPO/orbit/orbit/releases" | jq ".[0].created_at")"
-
-releaseNotes=$(curl -sH "$AUTH" "$GH_REPO/orbit/orbit/commits?since=$(curl -sH "$AUTH" "$GH_REPO/orbit/orbit/releases" | jq ".[0].created_at")" | jq ".[].commit.message")
+releaseNotes=$(curl -sH "$AUTH" "$GH_REPO/commits?since=$(curl -sH "$AUTH" "$GH_REPO/releases" | jq ".[0].created_at")" | jq ".[].commit.message")
 echo Release Notes: $releaseNotes
 
 echo Tag commit with the release tag
